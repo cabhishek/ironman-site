3b12c6cbc4f7ab3cdb9e166b6037b68cac9d20db
Abhishek Kapatkar <cabhishek@gmail.com>
2014-07-27 16:47:18 -0400

    create new races is not already present in DB

--- data/persistAthleteRace.js
+++ data/persistAthleteRace.js
@@ -5,9 +5,10 @@ var AthleteRace = require('./../models/athleteRace'),
     Log = require('log'),
     log = new Log('info')
 
-module.exports = function * persistAthleteRace(data) {
+module.exports = function* persistAthleteRace(data) {
 
-    var athlete = yield _fetchAthlete(data)
+    var athlete =
+        yield _fetchAthlete(data)
 
     if (athlete) {
 
@@ -32,16 +33,17 @@ module.exports = function * persistAthleteRace(data) {
     }
 }
 
-function * _fetchAthlete(data) {
+function* _fetchAthlete(data) {
 
-    var athlete = yield new Athlete({
-        'id': data.id
-    }).fetch()
+    var athlete =
+        yield new Athlete({
+            'id': data.id
+        }).fetch()
 
     return athlete
 }
 
-function * _fetchRace(raceData) {
+function* _fetchRace(raceData) {
     log.info("Getting race data for =>%s--%s", raceData.name, raceData.year)
 
     return yield new Race({
@@ -52,20 +54,21 @@ function * _fetchRace(raceData) {
 
 }
 
-function * _createAthlete(data) {
+function* _createAthlete(data) {
 
-    var athlete = yield Athlete.forge({
-        first_name: data.first_name,
-        last_name: data.last_name,
-        email: data.email
+    var athlete =
+        yield Athlete.forge({
+            first_name: data.first_name,
+            last_name: data.last_name,
+            email: data.email
 
-    }).save()
+        }).save()
 
     //Now go save races for that athlete
     return yield _persistAthleteRaceData(athlete, data)
 }
 
-function * _persistAthleteRaceData(athlete, data) {
+function* _persistAthleteRaceData(athlete, data) {
 
     // Add athlete_id since we save race data in parallel
     _.each(data.races, function(race) {
@@ -81,42 +84,59 @@ function * _persistAthleteRaceData(athlete, data) {
     }
 }
 
-function * persist(race) {
+function* persist(race) {
 
-    var ironmanRace = yield _fetchRace(race)
+    var ironmanRace =
+        yield _fetchRace(race)
 
-    if (ironmanRace) {
-        log.info("Ironman race id =>" + ironmanRace.id)
+    if (!ironmanRace) {
 
-        var athleteRace = yield new AthleteRace({
+        var qualifier =
+            yield new Race({
+                name: race.name,
+            }).fetch()
+
+        log.info("Race not found, so creating new one =>" + race.name + ' ' + race.year)
+
+        ironmanRace =
+            yield Race.forge({
+                name: race.name,
+                year: race.year,
+                qualifier_id: qualifier.attributes.qualifier_id,
+                race_type: qualifier.attributes.race_type,
+                month: qualifier.attributes.month,
+                type: qualifier.attributes.race_type == 'Ironman' ? 0 : 1
+            }).save()
+    }
+
+    log.info("Ironman race id =>" + ironmanRace.id)
+
+    var athleteRace =
+        yield new AthleteRace({
             'id': race._pivot_id
         }).fetch()
 
-        data = {
-            swim_time: race._pivot_swim_time,
-            run_time: race._pivot_run_time,
-            cycle_time: race._pivot_cycle_time,
-            final_time: race._pivot_final_time
-        }
+    data = {
+        swim_time: race._pivot_swim_time,
+        run_time: race._pivot_run_time,
+        cycle_time: race._pivot_cycle_time,
+        final_time: race._pivot_final_time
+    }
 
-        if (athleteRace) {
-            yield athleteRace.save(data, {
-                patch: true
-            })
-
-        } else {
-
-            //create a new record
-            _.extend(data, {
-                athlete_id: race.athlete_id,
-                race_id: ironmanRace.id,
-            })
-
-            yield AthleteRace.forge(data).save()
-        }
+    if (athleteRace) {
+        yield athleteRace.save(data, {
+            patch: true
+        })
 
     } else {
-        log.info("Race not found")
+
+        //create a new record
+        _.extend(data, {
+            athlete_id: race.athlete_id,
+            race_id: ironmanRace.id,
+        })
+
+        yield AthleteRace.forge(data).save()
     }
 
     return true
